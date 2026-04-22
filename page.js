dddf601319dce85bee22e7ccf0f64caca3ad24d2
skydigitaltr <ku_durmaz1@outlook.com>
2026-04-23 00:49:30 +0300

    Add files via upload

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{padding:40,fontFamily:'Arial'}}>
+      <h1>SKY DIGITAL Teklif Sitesi</h1>
+      <p>Kurulum başarılı 🎉</p>
+    </main>
+  );
+}
